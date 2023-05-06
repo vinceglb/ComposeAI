943777f56f5034e95f135e966c0f244b04cce451
Vincent Guillebaud <v.guillebaud@akawan.fr>
2023-05-07 01:47:20 +0200

    🍱 Update Android icons

--- shared/src/commonMain/kotlin/ui/images/AppImages.kt
+++ shared/src/commonMain/kotlin/ui/images/AppImages.kt
@@ -1,6 +1,6 @@
 package ui.images
 
 object AppImages {
-    const val composeAIDark = "drawable/ic_compose_ai.png"
-    const val composeAILight = "drawable/ic_compose_ai_light.png"
+    const val composeAIDark = "drawable/ic_launcher_playstore.png"
+    const val composeAILight = "drawable/ic_launcher_light_playstore.png"
 }
