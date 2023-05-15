f51b722c0955f27d9c3f9c9efba293944283723c
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2023-05-15 02:53:55 +0200

    🎨 Improve analyticsHelper import in App

--- androidApp/src/androidMain/kotlin/com/ebfstudio/appgpt/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/ebfstudio/appgpt/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.ebfstudio.appgpt
 
 import MainView
-import analytics.AnalyticsHelper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
-
-    private val analyticsHelper: AnalyticsHelper by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
         setContent {
-            MainView(
-                analyticsHelper = analyticsHelper
-            )
+            MainView()
         }
     }
+
 }
