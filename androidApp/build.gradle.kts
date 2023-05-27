6a7ea9e5db7c404177fb45b52dd98053723c21fd
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2023-05-27 15:57:56 +0200

    🔖 Version 1.2.1 - 17

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.ebfstudio.appgpt"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 16
-        versionName = "1.2.0"
+        versionCode = 17
+        versionName = "1.2.1"
     }
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
