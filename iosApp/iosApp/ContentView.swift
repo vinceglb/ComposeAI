0a324dc4415279c1052bff396d041a2c3e108e2d
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2023-06-05 23:55:44 +0200

    💄 Add explicit place to change the status bar color Make it so that Compose stretches to the bottom of the screen (no white border)

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -11,22 +11,9 @@ struct ComposeView: UIViewControllerRepresentable {
 }
 
 struct ContentView: View {
-    @Environment(\.colorScheme) var colorScheme: ColorScheme
-    
     var body: some View {
-        let bg = ColorKt.getBackgroundColor(isDarkTheme: colorScheme == .dark)
-        let color = Color(
-            red: bg.first?.doubleValue ?? 0,
-            green: bg.second?.doubleValue ?? 0,
-            blue: bg.third?.doubleValue ?? 0
-        )
-        
-        ZStack {
-            color.ignoresSafeArea()
-            
-            ComposeView()
-                .ignoresSafeArea(.keyboard)  // Compose has own keyboard handler
-        }
+        ComposeView()
+                .ignoresSafeArea(.all, edges: .bottom) // Compose has own keyboard handler
     }
 }
 
