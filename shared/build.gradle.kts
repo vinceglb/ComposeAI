9572d8f5e9f5c3f780d861d2dbf1fcb569ca261a
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2023-06-06 00:22:50 +0200

    ⚡️ Allow higher than 60hz framerates on CADisplayLink on iPhones

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -151,12 +151,11 @@ android {
 
     defaultConfig {
         minSdk = (findProperty("android.minSdk") as String).toInt()
-        targetSdk = (findProperty("android.targetSdk") as String).toInt()
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
