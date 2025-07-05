5b5d6e7593fe059151b8cb332e238964ea37b1ae
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-07-05 18:13:13 +0200

    💄 Enable edge-to-edge display in MainActivity

--- androidApp/src/main/kotlin/com/ebfstudio/appgpt/MainActivity.kt
+++ androidApp/src/main/kotlin/com/ebfstudio/appgpt/MainActivity.kt
@@ -4,12 +4,14 @@ import MainView
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
