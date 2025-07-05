b16249b161994fdb8342445b5e9af02735f30dd1
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-07-05 18:20:58 +0200

    🔖 Version 1.7.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ebfstudio.appgpt"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 29
-        versionName = "1.6.1"
+        versionCode = 30
+        versionName = "1.7.0"
     }
 
     buildFeatures {
